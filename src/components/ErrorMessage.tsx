c70ff17b1f262db1a933a70469d8449dc9e1fa0a
Krigle <130289154+Krigle@users.noreply.github.com>
2024-09-03 09:04:23 +0100

    updates

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,3 +1,7 @@
-export default function ErrorMessage({ message }) {
+interface ErrorMessageProps {
+  message: string;
+}
+
+export default function ErrorMessage({ message }: ErrorMessageProps) {
   return <div>{message}</div>;
 }
